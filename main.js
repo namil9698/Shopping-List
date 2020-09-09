840dfb28016eabe70db6d50c46d38b9d9f34f87c
namil9698 <69798566+namil9698@users.noreply.github.com>
2020-09-09 16:32:48 +0900

    수정!

--- main.js
+++ main.js
@@ -1,61 +1,78 @@
-const items = document.querySelector('.items');
-const input = document.querySelector('.footer__input');
-const addBtn = document.querySelector('.footer__button');
-
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
 
 function onAdd() {
-    const text = input.value;
-    if (text.trim() === "") {
-        input.focus();
-        return;
-    }
-    const item = createItem(text);
-    items.appendChild(item);
-    item.scrollIntoView({
-        block: 'center'
-    });
-    input.value = '';
+  const text = input.value;
+  if (text.trim() === "") {
     input.focus();
-
+    return;
+  }
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({
+    block: "center",
+  });
+  input.value = "";
+  input.focus();
 }
+
+let id = 0;
 
 function createItem(text) {
-    const itemRow = document.createElement('li');
-    itemRow.setAttribute('class', 'item__row');
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+    <div class="item" data-id="${id}">
+        <span class="item__name">${text}</span>
+        <button class="item__delete" >
+                <i class="fas fa-trash-alt" data-id="${id}"></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>`;
+  id++;
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text;
+  // const name = document.createElement('span');
+  // name.setAttribute('class', 'item__name');
+  // name.innerText = text;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete')
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'item__delete')
+  // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    });
+  // deleteBtn.addEventListener('click', () => {
+  //     items.removeChild(itemRow);
+  // });
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item__divider');
 
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
+  // item.appendChild(name);
+  // item.appendChild(deleteBtn);
 
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
 
-    return itemRow;
-
+  return itemRow;
 }
-addBtn.addEventListener('click', () => {
-    onAdd();
+addBtn.addEventListener("click", () => {
+  onAdd();
 });
 
-input.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        onAdd();
-    }
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    onAdd();
+  }
+});
+
+items.addEventListener("click", (e) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
 });
